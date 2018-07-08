f5ff08cea0dde964dca8a8fd8276b537c8f8a47e
Anderson Oliveira <andersonoliveira@macbook-pro-de-anderson.local>
2018-07-08 07:01:44 -0400

    Initial Commit

---
+++ EverPassTests/EverPassTests.swift
@@ -0,0 +1,36 @@
+//
+//  EverPassTests.swift
+//  EverPassTests
+//
+//  Created by Anderson Oliveira on 08/07/2018.
+//  Copyright © 2018 Anderson Oliveira. All rights reserved.
+//
+
+import XCTest
+@testable import EverPass
+
+class EverPassTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
